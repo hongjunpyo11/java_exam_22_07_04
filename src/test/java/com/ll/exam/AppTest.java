862ac3dd2161d5148dddabe3dc04d587229e8b2a
hongjunpyo11 <jpyo98@naver.com>
2022-07-05 13:33:36 +0900

    JUNIT   불러오기

---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,15 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void 더하기() {
+        int a = 10 + 5;
+        assertEquals(15, a);
+
+    }
+}
